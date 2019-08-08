51a0f2802214171d6aa9d69fb6735b6988d3a724
unknown <timotheus@list.ru>
2019-08-08 21:54:33 +0300

    'corsproblem'

--- src/s/beltol.js
+++ src/s/beltol.js
@@ -3,23 +3,48 @@
 
 // Минск 53.896765, 27.538642
 
-let req = 'http://calc.beltoll.by/proxy.ashx?http://localhost/arcgis/rest/services/BLR_RoutingService/MapServer/exts/RoutingSOE/solveRouteSimple?stops={"features":[';
 
 function beltol() {
+    let req = 'http://calc.beltoll.by/proxy.ashx?http://localhost/arcgis/rest/services/BLR_RoutingService/MapServer/exts/RoutingSOE/solveRouteSimple?stops=';
+    let features = '{"features":[';
 
-    let wrap = {};
-    let features = [];
     const inputs = document.querySelectorAll('.wrap .left input');
     inputs.forEach((e)=>{
         let coords = e.dataset.location.split(',');
         //console.log(coords[0]);
-        features.push({ geometry: {"x": coords[0], "y":coords[1], "spatialReference": {"wkid":4326} }})
+        features +='{"geometry":{"x":coords[0],"y":coords[1],"spatialReference":{"wkid":4326}}},';
         //req += JSON.stringify(wrap);
         //req
     });
 
-    console.log(features);
+    features += '"spatialReference":{"wkid":"4326"}}]}' ;
+    req = req.concat(req, features);
+    console.log(req);
+
+    fetchIt(req);
 
 }
 
+function fetchIt(req)
+{
+    fetch(req, {
+        mode: 'no-cors',
+        headers: {
+            //'Content-Type': 'application/javascript',
+            'Access-Control-Allow-Origin': "*",
+
+            "accept":"*/*",
+            "content-type":"application/x-www-form-urlencoded",
+            "pragma":"no-cache",
+            "x-requested-with":"XMLHttpRequest"
+        }
+    })
+        .then(response => response.text())
+        .then(data => {
+            console.log(data)
+        });
+}
+
+
 export { beltol };
+
