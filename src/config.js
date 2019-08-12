169215623b8713c1f587d06d007f508e507633da
aisteron <timotheus@list.ru>
2019-08-12 15:18:06 +0300

    'w12.08'

--- src/config.js
+++ src/config.js
@@ -1,19 +1,26 @@
 let config = {
     vehicles: {
         sedan: {
+
             "econom": {
+                "name": "эконом",
+                "className": "econom",
                 "seats" : 2,
                 "rate": 0.2,
                 "cons": 5,
                 "rent": 150
             },
             "business": {
+                "name": "бизнес",
+                "className": "business",
                 "seats": 2,
                 "rate": 0.3,
                 "cons": 7,
                 "rent": 200
             },
             "exec": {
+                "name": "представ.",
+                "className": "exec",
                 "seats": 2,
                 "rate": 0.7,
                 "cons": 12,
@@ -22,18 +29,24 @@ let config = {
         },
         van: {
             "minivan": {
+                "name": "минивэн",
+                "className": "minivan",
                 "seats" : 7,
                 "rate": 0.3,
                 "cons": 13,
                 "rent": 250
             },
             "econom": {
+                "name": "15 мест",
+                "className": "econom",
                 "seats": 15,
                 "rate": 0.4,
                 "cons": 13,
                 "rent": 300
             },
             "business": {
+                "name": "20 мест",
+                "className": "business",
                 "seats": 20,
                 "rate": 0.7,
                 "cons": 13,
@@ -42,18 +55,24 @@ let config = {
         },
         bus: {
             "standart": {
+                "name": "стандарт",
+                "className": "standart",
                 "seats" : 49,
                 "rate": 0.3,
                 "cons": 32,
                 "rent": 450
             },
             "55": {
+                "name": "55 мест",
+                "className": "55",
                 "seats": 55,
                 "rate": 0.3,
                 "cons": 32,
                 "rent": 500
             },
             "2floor": {
+                "name": "2 этажа",
+                "className": "2floor",
                 "seats": 70,
                 "rate": 0.4,
                 "cons": 35,
--- src/s/type.js
+++ src/s/type.js
@@ -1,3 +1,62 @@
 import {config} from "../config";
 
-//console.log(config.vehicles.bus)
+// изменения в первом селекторе
+let type = document.querySelector('select.type');
+
+type.addEventListener("change",(e)=>{
+    drawSelectDetails(type.options[type.selectedIndex].classList[0]);
+});
+
+
+function drawSelectDetails(className) {
+
+    let selectDetails = document.querySelector('select.details');
+    selectDetails.innerHTML = '';
+
+    let details = config.vehicles[className];
+
+    Object.values(details).forEach((e) => {
+        selectDetails.innerHTML += `<option class="${e.className}">${e.name}</option>`;
+    });
+
+    draw(selectDetails.querySelector('option:nth-of-type(1)').classList);
+
+}
+
+
+// первичная инициализация
+
+drawSelectDetails('sedan');
+
+/* первый селект с типом транспорта
+// второй селект - с подробностями
+// мы будем рисовать частями:
+   1. слушаем событие у первого селекта. по change рисуем содержимое 2-го селекта
+   2. и вызываем функцию отрисовки остального
+
+   у options второго селекта создаем слушатель (они ведь тоже могут меняться )
+   и вновь вызываем функцию draw отрисовки остального
+
+   */
+
+// слушатель у второго селекта details
+
+document.addEventListener("change", function(e) {
+    if(e.target.classList.value === 'details') {
+        draw(e.target.options[e.target.selectedIndex].classList[0])
+    }
+})
+
+
+function draw(className)
+{
+    let firstSelectValue = document.querySelector('select.type').options[document.querySelector('select.type').selectedIndex].classList[0];
+
+    let typeMe = config.vehicles[firstSelectValue][className];
+
+    document.querySelector('#seats').value = typeMe.seats;
+    document.querySelector('#rate').value = typeMe.rate;
+    document.querySelector('#cons').value = typeMe.cons;
+    document.querySelector('#rent').value = typeMe.rent;
+
+}
